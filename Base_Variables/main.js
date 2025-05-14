655cdb3bed39d8b5995631bb3e7130d531474795
Cristian Franco <cfrancobedoya@gmail.com>
2025-05-13 19:56:41 -0500

    first commit

---
+++ Base_Variables/main.js
@@ -0,0 +1,13 @@
+var userName = "John";
+var age = 20;
+var isDeveloper = true;
+var month;
+var variableNull = null;
+console.log("Hello, World!");
+console.log(userName);
+console.log(age);
+console.log(isDeveloper);
+console.log("Initial month", month);
+month = "January";
+console.log("Updated month", month);
+console.log(variableNull);
---
+++ Base_Variables/main.ts
@@ -0,0 +1,14 @@
+let userName: string = "John";
+let age: number = 20;
+let isDeveloper: boolean = true;
+let month: string | undefined;
+let variableNull: null = null;
+
+console.log("Hello, World!");
+console.log(userName);
+console.log(age);
+console.log(isDeveloper);
+console.log("Initial month", month);
+month = "January";
+console.log("Updated month", month);
+console.log(variableNull);
