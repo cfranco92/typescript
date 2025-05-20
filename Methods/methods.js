a454135f29380aa85d4f6a5692d1dc2c4a3ea66a
Cristian Franco <cfrancobedoya@gmail.com>
2025-05-19 19:29:02 -0500

    [MASTER] Add methods with types for params and return values

---
+++ Methods/methods.js
@@ -0,0 +1,9 @@
+function printMessage(message) {
+    console.log(message);
+}
+printMessage("Hello, I am a message");
+function add(a, b) {
+    return a + b;
+}
+var result = add(1, 2);
+console.log("Your result is ".concat(result));
---
+++ Methods/methods.ts
@@ -0,0 +1,13 @@
+function printMessage(message: string): void {
+  console.log(message);
+}
+
+printMessage("Hello, I am a message");
+
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+let result: number = add(1, 2);
+
+console.log(`Your result is ${result}`);
